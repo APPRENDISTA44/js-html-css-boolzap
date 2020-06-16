fe52308cbc485532cd37aeddb007631a31e0e69d
Luca Isidoro <apprendista44@gmail.com>
2020-06-16 18:19:05 +0200

    aggiunta parte javascript, da correggere problema con messaggio inviato troppo lungo

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
+  $(document).on('click','span.invio',
+  function () {
+    var testo = $('#text').val();
+    console.log(testo);
+    var cloneVerde = $('.riutilizzabili .template.green').clone();
+    cloneVerde.removeClass('hidden');
+    console.log(cloneVerde);
+    console.log(cloneVerde.find('p').text());
+    cloneVerde.find('p').text(testo);
+    $('div.chat').append(cloneVerde);
 
-});
+  })
+  });
