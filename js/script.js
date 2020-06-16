67596a95d08a3f9acb54cae7cbaa856d080753c7
Luca Isidoro <apprendista44@gmail.com>
2020-06-16 18:40:17 +0200

    inserita una funzione scrittura e aggiunta possibilità invio con tasto

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 $(document).ready(function() {
   $(document).on('click','span.invio',
   function () {
+    scrittura();
+  });
+  $('#text').keypress(function (event) {
+    if (event.which == 13) {
+      scrittura();
+    }
+  })
+
+  //funzione di scrittura
+  function scrittura() {
     var testo = $('#text').val();
     console.log(testo);
     var cloneVerde = $('.riutilizzabili .template.green').clone();
@@ -9,6 +19,5 @@ $(document).ready(function() {
     console.log(cloneVerde.find('p').text());
     cloneVerde.find('p').text(testo);
     $('div.chat').append(cloneVerde);
-
-  })
+  }
   });
